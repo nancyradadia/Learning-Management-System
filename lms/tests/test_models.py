9d7933b111ba89a6dd0adf87485fccb4ce92506e
nancyradadia <narofficial08@gmail.com>
2021-05-04 19:25:34 +0530

    Issues fixed

--- lms/tests/test_models.py
+++ lms/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lms.models import CustomUser
+from lms.models import CustomUser,Faculty
 
 class CustomUserModelTest(TestCase):
     @classmethod
@@ -23,3 +23,24 @@ class CustomUserModelTest(TestCase):
         author = CustomUser.objects.get(email='harvish.j@ahduni.edu.in')
         max_length = author._meta.get_field('identification').max_length
         self.assertEqual(max_length, 10)
+
+
+class FacultyModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Faculty.objects.create(f_name='Dhaval',l_name='Patel',f_id='AU1841102',email_id='dhaval.p@ahduni.edu.in')
+
+    def test_first_name_label(self):
+        author = Faculty.objects.get(email_id='dhaval.p@ahduni.edu.in')
+        field_label = author._meta.get_field('f_name').verbose_name
+        self.assertEqual(field_label, 'f name')
+
+    def test_string_representation(self):
+        entry = Faculty(email_id="dhaval.p@ahduni.edu.in")
+        self.assertEqual(str(entry), entry.email_id)
+    #
+    # def test_identification_max_length(self):
+    #     author = CustomUser.objects.get(email='harvish.j@ahduni.edu.in')
+    #     max_length = author._meta.get_field('identification').max_length
+    #     self.assertEqual(max_length, 10)
