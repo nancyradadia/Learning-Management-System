c827188b5fb02617571c33a07205985df26ac530
nancyradadia <narofficial08@gmail.com>
2021-05-22 11:36:08 +0530

    Managed Secret Key

--- src/Learning_Management_System/settings.py
+++ src/Learning_Management_System/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 from pathlib import Path
 import os
 
@@ -20,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't(&o97vh$wk10p1o)^z_6%_7*b36uiih#n8np-3-y86=r!982e'
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
